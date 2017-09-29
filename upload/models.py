f862b5abbb72413a8249b98347999085d56d7cd3
Kim <wnkim@ebay.com>
2017-09-29 09:39:27 +0900

    first commit

---
+++ upload/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class UploadFileModel(models.Model):
+    title = models.TextField(default='')
+    file = models.FileField(null=True)
---
+++ upload/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import UploadFileForm
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload/upload.html', {'form': form})
